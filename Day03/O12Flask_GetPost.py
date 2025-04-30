7494760879ff200f4156e5d96f7603512cfabf95
Kiran <colkiran@gmail.com>
2025-04-30 17:26:35 +0530

    commit

---
+++ Day03/O12Flask_GetPost.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template, redirect, url_for, flash, request
+
+app = Flask(__name__)
+app.secret_key = "123@hello"
+flag = False
+
+@app.route("/")
+def home():
+    return render_template("index04.html", uname="Joseph")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    global flag
+    if request.method == 'POST':
+        user = request.form['nm']
+        flag = True
+        return redirect(url_for("user", usr=user))
+    else:
+        flag= False
+        if flag == False:
+            flash("You have used GET Method.....")
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    global flag
+    if flag:
+        flash("You have used the POST Method.......")
+    return render_template("result.html", usr=usr)
+
+@app.route("/index")
+def index():
+    return render_template("index03.html", usrnm='Tyson', content="Fruits available in all seasons", fruits = ['Apple', 'Orange', 'Grapes', 'Banana', 'Pineapple', 'Strawberry', 'Watermelon', 'Blueberry', 'Mango'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
